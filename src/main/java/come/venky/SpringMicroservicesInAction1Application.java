139237cfeedfc1487f5fcb181a5d10aa07707b28
Venkatesh Kadali <vkadali@inmoment.com>
2020-06-19 19:25:09 -0600

    creates maven project

---
+++ src/main/java/come/venky/SpringMicroservicesInAction1Application.java
@@ -0,0 +1,13 @@
+package come.venky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesInAction1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesInAction1Application.class, args);
+	}
+
+}
---
+++ src/test/java/come/venky/SpringMicroservicesInAction1ApplicationTests.java
@@ -0,0 +1,13 @@
+package come.venky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesInAction1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
